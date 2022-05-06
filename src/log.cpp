1c933fd7d917ff44ab99225b53e02c91eb103bf0
Paweł Grabowski <gprabowski@outlook.com>
2022-05-06 20:35:31 +0200

    log fix

--- src/log.cpp
+++ src/log.cpp
@@ -5,7 +5,7 @@ std::shared_ptr<spdlog::logger> log::core_logger;
 
 void log::init() {
   spdlog::set_pattern("%^[%T] %n: %v%$");
-  core_logger = spdlog::stdout_color_mt("TinyCAD");
+  core_logger = spdlog::stdout_color_mt("PUMA");
   core_logger->set_level(spdlog::level::trace);
   GK2_PUMA_INFO("Initialized log!");
 }
